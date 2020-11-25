ec341f735abeb558915e77094d7ce1a9e4d1f4c7
Illan <illan.sempere@gmail.com>
2020-11-24 16:04:42 -0800

    Lab7 done

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,53 @@
-const courseList = [ 
-    course1 = {
-        "code": "ACIT 1515",
-        "name": "Scripting for IT"
-    },
-    course2 = {
-        "code": "COM 1116",
-        "name": "Business Communications"
-    },
-    course3 = {
-        "code": "ACIT 1620",
-        "name": "Web Fundamental Technologies"
-    }];
+function createCourseArray() { 
+    const courseList = [
+        course1 = {
+            "code": "ACIT 1515 - Scripting for IT",
+            "date": "Fall 2020"
+        },
+        course2 = {
+            "code": "COMM 1116 - Business Communications",
+            "date": "Fall 2020"
+        },
+        course3 = {
+            "code": "ACIT 1620 - Web Fundamental Technologies",
+            "date": "Fall 2020"
+        }];
+    
+    return courseList
+};
 
+
+function findCourse(courseList) {
 let number;
 do{
     number = prompt("Enter your course code: ");
 }
 while (isNaN(Number(number)) || number.length !== 4);
+    count = 0;
     for (let courses of courseList) {
-        if (number === courses.code.substring(5,9)) {
-            console.log(`Yes I am taking the course: ${courses.code} - ${courses.name}`);
-        } else {
-            courseList.push(course4 = {"code": `"${number}"`, "name": "null"});
+        if (courses.code.includes(number)) {
+            console.log(`Yes, I am taking the course: ${courses.code}`);
+            document.getElementById(number).style.backgroundColor = "green";
+            break;
+        } 
+        count += 1
+        
+        if (count === 3) {
+            console.log(`No, I am not taking the course ACIT-${number}.`)
+            var para = document.createElement("P");
+            para.innerText = `ACIT-${number}`;
+            document.body.appendChild(para)
+            var para2 = document.createElement("P");
+            para2.innerText = "Fall 2020";
+            document.body.appendChild(para2)
+            var para3 = document.createElement("P");
+            para3.innerText = "N/A";
+            document.body.appendChild(para3)
+            break;
         }
+
     }
+
+}
+
+findCourse(createCourseArray());
